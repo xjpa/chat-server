725974fed90545e756bb366befbcd0300439e1a8
xjpa <xjpa@proton.me>
2024-07-12 01:02:13 +0800

    add broadcasting, add readme

---
+++ README.md
@@ -0,0 +1,15 @@
+To run
+
+```bash
+$ racket chat-server.rkt
+```
+
+Then start a client, go open a terminal and type
+
+```
+$ nc localhost 8080
+```
+
+Then enter whatever message you want to broadcast and press enter
+
+You can open up another terminal so theres multiple clients connected
--- chat-server.rkt
+++ chat-server.rkt
@@ -1,17 +1,39 @@
 #lang racket
 
+(require racket/tcp)
+
+(define clients '())
+
+(define (broadcast message sender)
+  (printf "broadcasting: ~a~n" message)
+  (for ([client (in-list clients)])
+    (unless (eq? client sender)
+      (with-handlers ([exn:fail? (lambda (exn) 
+                                   (printf "error sending to a client: ~a~n" (exn-message exn)))])
+        (displayln message (cdr client))
+        (flush-output (cdr client))))))
+
+(define (handle-client in out)
+  (let ([client (cons in out)])
+    (set! clients (cons client clients))
+    (printf "new client connected. total clients: ~a~n" (length clients))
+    (let loop ()
+      (with-handlers ([exn:fail? (lambda (exn) 
+                                   (printf "client disconnected: ~a~n" (exn-message exn)))])
+        (define message (read-line in))
+        (printf "received message: ~a~n" message)
+        (when (not (eof-object? message))
+          (broadcast (format "client says: ~a" message) client)
+          (loop))))
+    (set! clients (remove client clients))
+    (printf "client disconnected. remaining clients: ~a~n" (length clients))))
+
 (define (start-server port)
-  (define listener (tcp-listen port))
+  (define listener (tcp-listen port 5 #t))
+  (printf "chat server is running on port ~a~n" port)
   (let loop ()
     (define-values (in out) (tcp-accept listener))
     (thread (lambda () (handle-client in out)))
     (loop)))
-
-(define (handle-client in out)
-  (let loop ()
-    (define message (read-line in))
-    (unless (eof-object? message)
-      (displayln message)
-      (loop))))
 
 (start-server 8080)
